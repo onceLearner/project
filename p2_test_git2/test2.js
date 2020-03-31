2789b4726e7aaf69792b50cfabe2228616b1050e
OnceLearner <redaamir972@gmail.com>
2020-03-31 02:48:59 +0100

    those are 3 commit from new Feature

--- p2_test_git2/test2.js
+++ p2_test_git2/test2.js
@@ -1,3 +1,4 @@
 console.log(" helllo world thsi me learing to use github !");
 console.log("hello world this a  branche ");
 console.log("this a 2 feature in the new feature breance");
+console.log("thsi 3 test ");
